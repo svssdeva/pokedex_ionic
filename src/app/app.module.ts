472f4676e23582e0aaebf4110c6773547dc4ff7f
deveshwar.jaiswal <deveshwar.jaiswal996@gmail.com>
2021-04-18 10:36:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+// @ts-ignore
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StoreModule.forRoot(reducers, {
+      metaReducers
+    })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
